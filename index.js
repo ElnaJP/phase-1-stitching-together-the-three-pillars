d8babaa2431976d4dffbd806e9923fca391276c1
Elna JP <eriosdd@gmail.com>
2022-07-29 11:54:50 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+function likeCallback(e) {
+     
+      alert("You notified the server!");
+     
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    }
+}
+
+   function error(error) {
+      alert("Something went wrong!");
+ function likeCallback(e) {
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+ }
+}
